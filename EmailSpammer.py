f32d61853b1a5da40412c6655b3774dcda1415c9
Nemo-5 <stickman5464@gmail.com>
2021-06-14 19:12:02 +1000

    whole renovation

--- EmailSpammer.py
+++ EmailSpammer.py
@@ -1,6 +1,6 @@
 import smtplib
-
 from email.message import EmailMessage
+
 
 selected = ""
 selectedPass = ""
@@ -10,44 +10,68 @@ password1 = "thisIsSmurf1234"
 
 bot2 = "pinksmurf221@gmail.com"
 
+class bcolors:
+    Green = '\033[92m' #GREEN
+    Yellow = '\033[93m' #YELLOW
+    Red = '\033[91m' #RED
+    Magneta = '\u001b[35m'#Magneta
+    Cyan = '\u001b[36m' #Cyan
+    Blue = '\u001b[34m'
+
+    Cyan_High_Bold = "\033[1;96m"
+    Reset = '\033[0m' #RESET COLOR
+
 #start screen
-print("░░░░░░░░████████████████░░░░░░░")
-print("░░░░░█████████████████████░░░░░")
-print("░░░░████████████████████████░░░")
-print("░░░██████████████████████████░░")
-print("░░█████████████████████████████")
-print("░░███████████▀░░░░░░░░░████████")
-print("░░███████████░░░░░░░░░░░░░░░███")
-print("░████████████░░░░░░░░░░░░░░░░██")
-print("░█░░███████░░░░░░░░░░░▄▄░░░░░██")
-print("█░░░░█████░░░░░░▄███████░░██░░█")
-print("█░░█░░░███░░░░░██▀▀░░░░░░░░██░█")
-print("█░░░█░░░░░░░░░░░░▄██▄░░░░░░░███")
-print("█░░▄█░░░░░░░░░░░░░░░░░░█▀▀█▄░██")
-print("█░░░░░░░░░░░░░░░░░░░░░░█░░░░██░")
-print("░███░░░░░░░░░░░░░░░░░░░█░░░░█░░")
-print("░░█░█░░░░░░░█░░░░░██▀▄░▄██░░░█░")
-print("░░█░█░░░░░░█░░░░░░░░░░░░░░░░░█░")
-print("░░░██░░░░░░█░░░░▄▄▄▄▄▄░░░░░░█░░")
-print("░░░██░░░░░░░█░░█▄▄▄▄░▀▀██░░█░░░")
-print("░░░██░░░░░░░█░░▀████████░░█░░░░")
-print("░░█░░█░░░░░░░█░░▀▄▄▄▄██░░█░░░░░")
-print("░░█░░░█░░░░░░░█░░░░░░░░░█░░░░░░")
-print("░█░░░░░█░░░░░░░░░░░░░░░░█░░░░░░")
-print("░░░░░░░░█░░░░░░█░░░░░░░░█░░░░░░")
-print("░░░░░░░░░░░░░░░░████████░░░░░░░")
 
-print("'Email Spammer' By: Leo Liu")
-input("Press 'Enter' to begin spam attack")
+laughingMan = ("""
+░░░░░░░░████████████████░░░░░░░
+░░░░░█████████████████████░░░░░
+░░░░████████████████████████░░░
+░░░██████████████████████████░░
+░░█████████████████████████████
+░░███████████▀░░░░░░░░░████████
+░░███████████░░░░░░░░░░░░░░░███
+░████████████░░░░░░░░░░░░░░░░██
+░█░░███████░░░░░░░░░░░▄▄░░░░░██
+█░░░░█████░░░░░░▄███████░░██░░█
+█░░█░░░███░░░░░██▀▀░░░░░░░░██░█
+█░░░█░░░░░░░░░░░░▄██▄░░░░░░░███
+█░░▄█░░░░░░░░░░░░░░░░░░█▀▀█▄░██
+█░░░░░░░░░░░░░░░░░░░░░░█░░░░██░
+░███░░░░░░░░░░░░░░░░░░░█░░░░█░░
+░░█░█░░░░░░░█░░░░░██▀▄░▄██░░░█░
+░░█░█░░░░░░█░░░░░░░░░░░░░░░░░█░
+░░░██░░░░░░█░░░░▄▄▄▄▄▄░░░░░░█░░
+░░░██░░░░░░░█░░█▄▄▄▄░▀▀██░░█░░░
+░░░██░░░░░░░█░░▀████████░░█░░░░
+░░█░░█░░░░░░░█░░▀▄▄▄▄██░░█░░░░░
+░░█░░░█░░░░░░░█░░░░░░░░░█░░░░░░
+░█░░░░░█░░░░░░░░░░░░░░░░█░░░░░░
+░░░░░░░░█░░░░░░█░░░░░░░░█░░░░░░
+░░░░░░░░░░░░░░░░████████░░░░░░░""")
 
-inp = input("What do you want to send? ")
+fiveLineOblique = ("""                                                                                              
+      ____                                        ___                                                              
+    //   / /                                    //   ) )                                                           
+   //____     _   __      ___     ( ) //       ((         ___      ___      _   __      _   __      ___      __    
+  / ____    // ) )  ) ) //   ) ) / / //          \\     //   ) ) //   ) ) // ) )  ) ) // ) )  ) ) //___) ) //  ) ) 
+ //        // / /  / / //   / / / / //             ) ) //___/ / //   / / // / /  / / // / /  / / //       //       
+//____/ / // / /  / / ((___( ( / / //       ((___ / / //       ((___( ( // / /  / / // / /  / / ((____   //    
+""")
+
+#print(bcolors.Blue+laughingMan + "\n")
+print(bcolors.Magneta + fiveLineOblique)
+print(bcolors.Magneta+"[Email Spammer] By: Leo Liu")
+input(bcolors.Green + "Press"+bcolors.Reset+" Enter" +bcolors.Green+" to begin spam attack" + bcolors.Reset)
+
+inp = input(bcolors.Yellow+"What do you want to send? "+bcolors.Reset)
 
 msg = EmailMessage()
 msg.set_content(inp) #text inside message
 
-subject = input("Enter the subject of the email ")
+subject = input(bcolors.Yellow+"Enter the subject of the email " + bcolors.Reset)
 try:
-    sender = input("Select which email you would like to send from: \n1 \n2 ")
+    sender = input(bcolors.Yellow+"Select which email you would like to send from: "+bcolors.Cyan+"\n1 \n2 \n" + bcolors.Reset)
     if sender == "1":
         selected = bot1
         selectedPass = password1
@@ -55,8 +79,8 @@ try:
         selected = bot2
         selectedPass = password1
 except:
-    sender = input("please enter a number: ")
-reciever = input("Enter the email that will recieve the spam: ")
+    sender = input(bcolors.Yellow+"please enter a number: " + bcolors.Reset)
+reciever = input(bcolors.Yellow+"Enter the email that will recieve the spam: " + bcolors.Reset)
 msg['Subject'] = subject #email subject
 msg['From'] = selected #sender
 msg['To'] = reciever  #reciever
@@ -67,15 +91,15 @@ client.login(selected, selectedPass)
 client.timeout = 10000
 
 try:
-    times = int(input("please enter how many times you will send the email: "))
+    times = int(input(bcolors.Yellow+"please enter how many times you will send the email: " + bcolors.Reset))
 except ValueError:
-    times = int(input("please enter a number: "))
+    times = int(input(bcolors.Yellow+"please enter a number: " + bcolors.Reset))
 
-print("sending...")
+print(bcolors.Cyan_High_Bold + "sending...")
 
 for number in range(times):
     client.send_message(msg)
     if number == (times - 1):
         t = str(times)
-        print("finished sending " + t + " emails!")
+        print(bcolors.Green+"Finished sending " + t + " emails!"+bcolors.Reset)
 
